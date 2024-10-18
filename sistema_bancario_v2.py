eba0fef4978a47823bc89efce6fe34524415691d
Luiz Silva <luizkarlus@hotmail.com>
2024-10-18 17:32:54 -0300

    DIO - Projeto sistema bancario

--- sistema_bancario_v2.py
+++ sistema_bancario_v2.py
@@ -2,11 +2,7 @@ saldo = 2000
 options = 0
 
 while options != 4:
-    options = int(
-        input(
-            "\nInforme uma opção: \n\n[1] Sacar \n[2] Extrato \n[3] Depositar \n[4] Sair \n\n"
-        )
-    )
+    options = int(input("\nInforme uma opção: \n\n[1] Sacar \n[2] Extrato \n[3] Depositar \n[4] Sair \n\n"))
 
     if options == 1:
         valor = int(input("Informe a quantia para o saque: "))
